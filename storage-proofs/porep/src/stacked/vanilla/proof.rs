0bea7dbab6cf0f213e2b2a4f28628a21c507822b
s1m0n21 <s1m0n21@hotmail.com>
2021-01-29 16:24:43 +0800

    optimized generate_tree_c

--- storage-proofs/porep/src/stacked/vanilla/proof.rs
+++ storage-proofs/porep/src/stacked/vanilla/proof.rs
@@ -389,6 +389,15 @@ impl<'a, Tree: 'static + MerkleTreeTrait, G: 'static + Hasher> StackedDrg<'a, Tr
         if settings::SETTINGS.use_gpu_column_builder {
             let mut device_bus_ids = device_bus_ids;
 
+            for bus_id in device_bus_ids {
+                let device = Device::by_bus_id(bus_id).unwrap();
+                let mem = (device.memory() as f64 / (1024 * 1024 * 1024) as f64 * 1_f64).round() / 1_f64;
+
+                if mem as u64 >= 8 {
+                    device_bus_ids.push(bus_id)
+                };
+            }
+
             if device_bus_ids.len() == 1 {
                 if settings::SETTINGS.tree_c_force_parallel {
                     device_bus_ids.push(device_bus_ids[0]);
